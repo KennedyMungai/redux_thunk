3cac08f4e4ad2080cee9423db554392ea4d71547
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 11:39:16 +0300

    Set the initialState to an empty array

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -4,11 +4,7 @@ import axios from "axios";
 
 const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
 
-const initialState = [
-    { id: '0', name: 'Dude Lebowski' },
-    { id: '1', name: 'Neil Young' },
-    { id: '2', name: 'Dave Gray' }
-]
+const initialState = []
 
 const usersSlice = createSlice({
     name: 'users',
