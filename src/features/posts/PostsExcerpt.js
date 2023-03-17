0614a516cdfba1b8f2f6dd62d7cf4ae7c84b89c2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 11:18:28 +0300

    Moved the article that renders various items to the page

--- src/features/posts/PostsExcerpt.js
+++ src/features/posts/PostsExcerpt.js
@@ -7,7 +7,15 @@ import TimeAgo from "./TimeAgo";
 const PostsExcerpt = ({ post }) =>
 {
     return (
-        <div>PostsExcerpt</div>
+        <article key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content.substring(0, 100)}</p>
+            <p className="postCredit">
+                <PostAuthor userId={post.userId} />
+                <TimeAgo timestamp={post.date} />
+            </p>
+            <ReactionButtons post={post} />
+        </article>
     )
 }
 
--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -22,22 +22,14 @@ const PostsList = () =>
 
     const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-    const renderedPosts = orderedPosts.map(post => (
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0, 100)}</p>
-            <p className="postCredit">
-                <PostAuthor userId={post.userId} />
-                <TimeAgo timestamp={post.date} />
-            </p>
-            <ReactionButtons post={post} />
-        </article>
-    ))
+    // const renderedPosts = orderedPosts.map(post => (
+
+    // ))
 
     return (
         <section>
             <h2>Posts</h2>
-            {renderedPosts}
+            {/* {renderedPosts} */}
         </section>
     )
 }
