dad77b4c6409c8b6ea2be4f23efcf4aa7a92059b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 10:31:19 +0300

    Redefined the initial state

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,44 +1,23 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { sub } from 'date-fns';
 
-const initialState = [
-    {
-        id: '1',
-        title: 'Learning Redux Toolkit',
-        content: "I've heard good things.",
-        date: sub(new Date(), { minutes: 10 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
-    },
-    {
-        id: '2',
-        title: 'Slices...',
-        content: "The more I say slice, the more I want pizza.",
-        date: sub(new Date(), { minutes: 5 }).toISOString(),
-        reactions: {
-            thumbsUp: 0,
-            wow: 0,
-            heart: 0,
-            rocket: 0,
-            coffee: 0
-        }
-    }
-]
+const initialState = {
+    post: [],
+    status: 'idle',     //'idle' | 'loading' | 'succeeded' | 'failed'
+    error: null
+}
 
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers: {
         postAdded: {
-            reducer(state, action) {
+            reducer(state, action)
+            {
                 state.push(action.payload)
             },
-            prepare(title, content, userId) {
+            prepare(title, content, userId)
+            {
                 return {
                     payload: {
                         id: nanoid(),
@@ -57,10 +36,12 @@ const postsSlice = createSlice({
                 }
             }
         },
-        reactionAdded(state, action) {
+        reactionAdded(state, action)
+        {
             const { postId, reaction } = action.payload
             const existingPost = state.find(post => post.id === postId)
-            if (existingPost) {
+            if (existingPost)
+            {
                 existingPost.reactions[reaction]++
             }
         }
