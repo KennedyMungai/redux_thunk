19fc5c89eb83e4ee9d97baff1bb08e14da099e3c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 11:05:06 +0300

    Imported useDispatch

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,10 +1,12 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
 
-const PostsList = () => {
+
+const PostsList = () =>
+{
     const posts = useSelector(selectAllPosts)
 
     const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
