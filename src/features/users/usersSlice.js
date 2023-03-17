304bfc849308addbd67563b7cb646bf47364a6f7
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 11:41:31 +0300

    Created the fetchUsers async thunk to fetch the user data from the api

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -6,6 +6,19 @@ const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
 
 const initialState = []
 
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => 
+{
+    try
+    {
+        const response = await axios.get(USERS_URL)
+        return [...response.data]
+    }
+    catch (err)
+    {
+        return err.message
+    }
+})
+
 const usersSlice = createSlice({
     name: 'users',
     initialState,
