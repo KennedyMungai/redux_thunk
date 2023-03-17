10f1e8613f12e8e453f5ee9a4f27287b03174f82
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-03-17 10:35:28 +0300

    Updated the initial state of the posts slice so that more data can be included in the state

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -14,7 +14,7 @@ const postsSlice = createSlice({
         postAdded: {
             reducer(state, action)
             {
-                state.push(action.payload)
+                state.posts.push(action.payload)
             },
             prepare(title, content, userId)
             {
@@ -39,7 +39,7 @@ const postsSlice = createSlice({
         reactionAdded(state, action)
         {
             const { postId, reaction } = action.payload
-            const existingPost = state.find(post => post.id === postId)
+            const existingPost = state.posts.find(post => post.id === postId)
             if (existingPost)
             {
                 existingPost.reactions[reaction]++
@@ -48,7 +48,7 @@ const postsSlice = createSlice({
     }
 })
 
-export const selectAllPosts = (state) => state.posts;
+export const selectAllPosts = (state) => state.posts.posts;
 
 export const { postAdded, reactionAdded } = postsSlice.actions
 
